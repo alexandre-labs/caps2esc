6daaed3cb9e358337605e56b1aee33f120bbf7a4
Alexandre Souza <alexandre_@fastmail.com>
2015-05-11 20:06:37 -0300

    complete case

--- debian_like.sh
+++ debian_like.sh
@@ -15,15 +15,22 @@ sudo make install
 echo "Copying xmodconf file"
 cp ./.xmodmap.conf ${HOME}/.xmodmap.conf
 
+echo "Copying caps2esc"
+cp ./caps2esc ${HOME}/caps2esc
+chmod +x ${HOME}/caps2esc
+
 echo "Do you wanna activate it now? [Y/N]"
 read activate_now
 
 case activate_now in
-	Y||y )
+	Y|y )
+		. ${HOME}/caps2esc
+		echo "Done!"
 		;;
-	N||n )
+	N|n )
+		echo "Ok, execute the file ${HOME} whenever you want."
 		;;
 	* )
-		echo "Invalid Option. I'll decide not activate it now. Execute '. ~/caps2esc'";
+		echo "Invalid Option. I'll decide not activate it now. Execute '. ${HOME}/caps2esc'";
 		exit 1;
 esac
--- slackware.sh
+++ slackware.sh
@@ -9,15 +9,22 @@ su -c make install
 echo "Copying xmodconf file"
 cp ./.xmodmap.conf ${HOME}/.xmodmap.conf
 
+echo "Copying caps2esc"
+cp ./caps2esc ${HOME}/.
+chmod +x ${HOME}/caps2esc
+
 echo "Do you wanna activate it now? [Y/N]"
 read activate_now
 
 case activate_now in
-	Y||y )
+	Y|y )
+		. ${HOME}/caps2esc
+		echo "Done!";
 		;;
-	N||n )
+	N|n )
+		echo "Ok, execute the file ${HOME}/caps2esc whenever you want. =]"
 		;;
 	* )
-		echo "Invalid Option. I'll decide not activate it now. Execute '. ~/caps2esc'";
+		echo "Invalid Option. I won't activate it now. Execute '. ~/caps2esc'";
 		exit 1;
 esac
