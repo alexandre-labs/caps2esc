f03e1c22454947b70fcac15815d24752f8029d47
Alexandre Souza <alexandre_@fastmail.com>
2015-05-13 09:19:28 -0300

    it should works

--- debian_like.sh
+++ debian_like.sh
@@ -2,10 +2,11 @@
 
 
 echo "Cloning Xcape"
-git submodule init --update --recursive
+git submodule update --init --recursive
 
 echo "Installing requirements - I'll need sudo"
 cd xcape
+git checkout master && git pull origin master
 sudo apt-get install git gcc make pkg-config libx11-dev libxtst-dev libxi-dev
 
 echo "Building and install"
@@ -13,24 +14,29 @@ make
 sudo make install
 
 echo "Copying xmodconf file"
-cp ./.xmodmap.conf ${HOME}/.xmodmap.conf
+cp xmodmap.conf ${HOME}/.xmodmap.conf
 
 echo "Copying caps2esc"
-cp ./caps2esc ${HOME}/caps2esc
+cp caps2esc ${HOME}/caps2esc
 chmod +x ${HOME}/caps2esc
 
 echo "Do you wanna activate it now? [Y/N]"
 read activate_now
 
-case activate_now in
-	Y|y )
+case ${activate_now} in
+	Y)
 		. ${HOME}/caps2esc
 		echo "Done!"
+		echo "Press any key"
+		read foo
 		;;
-	N|n )
-		echo "Ok, execute the file ${HOME} whenever you want."
+	N)
+		echo "Ok, execute the file ${HOME}/caps2esc whenever you want."
+		echo "Press any key"
+		read foo
 		;;
-	* )
-		echo "Invalid Option. I'll decide not activate it now. Execute '. ${HOME}/caps2esc'";
-		exit 1;
+	*)
+		echo "Invalid Option. I will not activate it now. Execute '. ${HOME}/caps2esc'";
+		echo "Press any key"
+		read foo
 esac
