42df71a0f89465d13a27576cc3a38612295b7034
Alexandre Souza <alexandre_@fastmail.com>
2015-05-13 09:14:33 -0300

    it works

--- slackware.sh
+++ slackware.sh
@@ -1,30 +1,38 @@
 #!/bin/sh
 
 
-git submodule init --update --recursive
+git submodule update --init --recursive
 cd xcape
+git checkout master && git pull origin master
 make
-su -c make install
+sudo make install
+
+cd ..
 
 echo "Copying xmodconf file"
-cp ./.xmodmap.conf ${HOME}/.xmodmap.conf
+cp xmodmap.conf ${HOME}/.xmodmap.conf
 
 echo "Copying caps2esc"
-cp ./caps2esc ${HOME}/.
+cp caps2esc ${HOME}/.
 chmod +x ${HOME}/caps2esc
 
 echo "Do you wanna activate it now? [Y/N]"
 read activate_now
 
-case activate_now in
-	Y|y )
+case ${activate_now} in
+	Y)
 		. ${HOME}/caps2esc
 		echo "Done!";
+		echo "Press any key"
+		read foo
 		;;
-	N|n )
+	N)
 		echo "Ok, execute the file ${HOME}/caps2esc whenever you want. =]"
+		echo "Press any key"
+		read foo
 		;;
-	* )
+	*)
 		echo "Invalid Option. I won't activate it now. Execute '. ~/caps2esc'";
-		exit 1;
+		echo "Press any key"
+		read foo
 esac
