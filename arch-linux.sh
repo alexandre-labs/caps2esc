253f2c9e94dd042a667f540a294f012517aaefb8
Alexandre <0x41e@protonmail.ch>
2016-04-30 17:06:47 -0300

    adding arch-linux

---
+++ arch-linux.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# it works! Slackware64 14.1
+
+git submodule update --init --recursive
+cd xcape
+git checkout master && git pull origin master
+make
+sudo make install
+
+cd ..
+
+echo "Copying xmodconf file"
+cp xmodmap.conf ${HOME}/.xmodmap.conf
+
+echo "Copying caps2esc"
+cp caps2esc ${HOME}/.
+chmod +x ${HOME}/caps2esc
+
+echo "Do you wanna activate it now? [Y/N]"
+read activate_now
+
+case ${activate_now} in
+	Y)
+		. ${HOME}/caps2esc
+		echo "Done!";
+		echo "Press any key"
+		read foo
+		;;
+	N)
+		echo "Ok, execute the file ${HOME}/caps2esc whenever you want. =]"
+		echo "Press any key"
+		read foo
+		;;
+	*)
+		echo "Invalid Option. I won't activate it now. Execute '. ~/caps2esc'";
+		echo "Press any key"
+		read foo
+esac
